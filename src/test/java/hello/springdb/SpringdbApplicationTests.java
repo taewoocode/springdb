38fe71a1deebddbfc3a3b39bfe0ed6848f6f75b5
taewoocode <qkrxodn6035@gmail.com>
2024-07-05 22:32:02 +0900

    first commit

---
+++ src/test/java/hello/springdb/SpringdbApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.springdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringdbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
